47ac80e112c24e0bdbdd47f0a61ca0d744009a1f
William Getz <willegetz@gmail.com>
2015-10-21 07:03:48 +0000

    Initial checkin with Obsidian Portal OAuth included.

---
+++ Caldera/Caldera/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Caldera.Startup))]
+namespace Caldera
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
