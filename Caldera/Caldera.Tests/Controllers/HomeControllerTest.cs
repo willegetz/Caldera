e125874eb1a940a2c4637897d588c41c8cfe33f4
William Getz <willegetz@gmail.com>
2015-10-28 07:01:46 +0000

    Added NUnit

--- Caldera/Caldera.Tests/Controllers/HomeControllerTest.cs
+++ Caldera/Caldera.Tests/Controllers/HomeControllerTest.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Caldera;
+﻿using System.Web.Mvc;
 using Caldera.Controllers;
+using NUnit.Framework;
 
 namespace Caldera.Tests.Controllers
 {
-    [TestClass]
+    [TestFixture]
     public class HomeControllerTest
     {
-        [TestMethod]
+        [Test]
         public void Index()
         {
             // Arrange
@@ -25,7 +20,7 @@ namespace Caldera.Tests.Controllers
             Assert.IsNotNull(result);
         }
 
-        [TestMethod]
+        [Test]
         public void About()
         {
             // Arrange
@@ -38,7 +33,7 @@ namespace Caldera.Tests.Controllers
             Assert.AreEqual("Your application description page.", result.ViewBag.Message);
         }
 
-        [TestMethod]
+        [Test]
         public void Contact()
         {
             // Arrange
